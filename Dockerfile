d2609e745db2ae608e96b38ff79d0c17140b63e7
Igor <idyurev@gmail.com>
2021-12-17 21:24:01 +0300

    v0.2

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,20 @@
+# Build the manager binary
+FROM golang:1.16 as builder
+
+WORKDIR /workspace
+# Copy the Go Modules manifests
+COPY go.mod go.mod
+COPY go.sum go.sum
+# cache deps before building and copying source so that we don't need to re-download as much
+# and so that source changes don't invalidate our downloaded layer
+RUN go mod download
+
+# Copy the go source
+COPY cmd/main.go main.go
+
+# Build
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GO111MODULE=on go build -a -o dummy-web-server main.go
+
 FROM registry.access.redhat.com/ubi8/ubi:latest
-ADD dummy-web-server /bin
+COPY --from=builder  /workspace/dummy-web-server /bin/
 ENTRYPOINT "/bin/dummy-web-server"
--- cmd/main.go
+++ cmd/main.go
@@ -2,31 +2,91 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"log"
-	"net/http"
-	"os"
 )
 
-func main() {
+const (
+	prefix         = "DUMMY_SRV"
+	defaultNetAddr = "0.0.0.0"
+	defaultNetPort = "8080"
+)
 
-	listenerPort := os.Getenv("HTTP_PORT")
-	if listenerPort == "" {
-		listenerPort = "8080"
+var (
+	config            *Config
+	xForwardedHeaders = []string{
+		"Forwarded",
+		"X-Forwarded-For",
+		"X-Forwarded-Host",
+		"X-Forwarded-Port",
+		"X-Forwarded-Proto",
+		"X-Forwarded-Proto-Version",
+		"X-Real-Ip",
+		"Host",
 	}
-	listenerAddr := os.Getenv("HTTP_ADDR")
+)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if len(body) == 0 {
-			body = []byte("Empty request body!!")
-		}
-		fmt.Fprintf(w, "Hello from %s\n Request body: %s\n", r.Host, body)
+func init() {
+	var err error
+	config, err = NewConfig(prefix)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+}
+
+type Config struct {
+	env *viper.Viper
+}
+
+func NewConfig(prefix string) (*Config, error) {
+	config := &Config{}
+	env := viper.New()
+	env.SetDefault("net_addr", defaultNetAddr)
+	env.SetDefault("net_port", defaultNetPort)
+	env.AllowEmptyEnv(false)
+	env.SetEnvPrefix(prefix)
+	env.AutomaticEnv()
+	config.env = env
+	return config, nil
+}
+
+func (config *Config) NetAddr() string {
+	return config.env.GetString("net_addr")
+}
+func (config *Config) NetPort() string {
+	return config.env.GetString("net_port")
+}
+
+func (config *Config) ListenerAddr() string {
+	return fmt.Sprintf("%s:%s", config.NetAddr(), config.NetPort())
+}
+
+func headersInfoHandler() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var headers = make(map[string]interface{}, 0)
+		for _, headerName := range xForwardedHeaders {
+			headerVal := ctx.GetHeader(headerName)
+			if headerVal != "" {
+				headers[headerName] = headerVal
+			}
+		}
+		r := map[string]interface{}{"headers": headers, "client_ip": ctx.ClientIP()}
+		ctx.JSON(200, r)
+	}
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
 	})
-	listenerContext := fmt.Sprintf("%s:%s", listenerAddr, listenerPort)
-	http.ListenAndServe(listenerContext, nil)
+	r.GET("/headers", headersInfoHandler())
+	if err := r.Run(config.ListenerAddr()); err != nil {
+		log.Fatal(err)
+	}
 }
